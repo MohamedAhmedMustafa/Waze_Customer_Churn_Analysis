837bc1c3013f7881a089467d20d2c6a4f5d802d9
Mohamed Ahmed Mustafa <mohamedahmedmustafa22@gmail.com>
2024-10-12 22:56:30 +0300

    Update App.py

--- App.py
+++ App.py
@@ -40,7 +40,8 @@ if uploaded_file is not None:
         if num_columns < 11:
             st.error("Uploaded file must contain at least 11 columns.")
         else:
-            
+            for col in test_data.select_dtypes(include=['object']).columns:
+            test_data[col] = pd.Categorical(test_data[col]).codes
             # First, use XGBoost to predict if the ECG is abnormal
             if st.button("Predict"):
                 # Clear previous results
