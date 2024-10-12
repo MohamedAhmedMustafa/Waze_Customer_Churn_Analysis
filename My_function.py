dde2a78cb2767c53f8516950099dde67a97a5309
Mohamed Ahmed Mustafa <mohamedahmedmustafa22@gmail.com>
2024-10-12 22:34:52 +0300

    Update My_function.py

--- My_function.py
+++ My_function.py
@@ -1,16 +1,12 @@
 import pandas as pd
 def preprocessing_df(df):
-  #df['device'] = pd.Categorical(df['device']).codes
+  df['device'] = pd.Categorical(df['device']).codes
   df = df.drop(columns=['ID'])
   df = df.dropna()
-  df1 = df.drop(columns=['label'])
-  df2 = df['label']
-  percentiles_99 = df1.quantile(0.95)
+  percentiles_99 = df.quantile(0.95)
   # Filter out records where values exceed the 99th percentile in any column
-  df_filtered = df1[(df1 <= percentiles_99).all(axis=1)]
+  df_filtered = df[(df <= percentiles_99).all(axis=1)]
   df = df_filtered
-  df['label'] = df2
-  #df = df.drop(columns=['device'])
+  df = df.drop(columns=['device'])
   df = df.dropna()
-  df['label'] = pd.Categorical(df['label']).codes
   return df
