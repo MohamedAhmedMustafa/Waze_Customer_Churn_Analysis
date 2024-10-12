70368922f3d5b49038cafe1ab18f4a0777bbdd8a
Mohamed Ahmed Mustafa <mohamedahmedmustafa22@gmail.com>
2024-10-12 23:02:29 +0300

    Add files via upload

--- App.py
+++ App.py
@@ -1,3 +1,8 @@
+import pip
+pip.main(['install','cmake'])
+pip.main(['install','pyarrow'])
+pip.main(['install','streamlit'])
+from My_function import preprocessing_df
 import streamlit as st
 import pandas as pd
 import pickle
@@ -6,16 +11,19 @@ import zipfile
 # Load models
 @st.cache_resource
 def load_models():
-    with zipfile.ZipFile('/mount/src/waze_customer_churn_analysis/ChurnModelandPreprocessing.zip', 'r') as zip_ref:
+    with zipfile.ZipFile('ChurnModelandPreprocessing.zip', 'r') as zip_ref:
         zip_ref.extractall()
 
     with open('Churn_model.pkl', 'rb') as f:
         Churn_model = pickle.load(f)
 
-    return Churn_model 
+    with open('preprocessing.pkl', 'rb') as f:
+        loaded_func = pickle.load(f)
+
+    return Churn_model ,loaded_func 
 
 # Load the models
-Churn_model = load_models()
+Churn_model ,preprocessing_df = load_models()
 
 # Streamlit App Code
 st.title("Waze Customer Churn Analysis")
@@ -30,35 +38,32 @@ if uploaded_file is not None:
     try:
         # Read the uploaded CSV file
         test_data = pd.read_csv(uploaded_file, header=None)
-        test_data.columns = ['activity_days', 'drives', 'driving_days', 'n_days_after_onboarding', 
-                             'total_sessions', 'sessions', 'total_navigations_fav2', 
-                             'total_navigations_fav1', 'driven_km_drives', 'duration_minutes_drives']
-        
         num_columns = test_data.shape[1]
 
-        # Check for the expected number of columns
-        if num_columns < 13:
-            st.error("Uploaded file must contain exactly 13 columns for features.")
+        if num_columns < 12:
+            st.error("Uploaded file must contain at least 12 columns.")
         else:
-            for col in test_data.select_dtypes(include=['object']).columns:
-                test_data[col] = pd.Categorical(test_data[col]).codes
-            
-            # First, use the model to predict churn
+            # Use only the first 100 columns if there are more
+            test_data = preprocessing_df(test_data)
+            X_test = test_data.iloc[:, :]
+
+            # First, use XGBoost to predict if the ECG is abnormal
             if st.button("Predict"):
                 # Clear previous results
                 results_placeholder.empty()
 
-                # Predict using the model
-                binary_pred = Churn_model.predict(test_data)
+                # Predict using XGBoost
+                binary_pred = Churn_model.predict(X_test)
                 results_data = []
 
-                # Collect the results
-                for pred in binary_pred:
+                # Loop through each prediction and collect the result
+                for idx, pred in enumerate(binary_pred):
                     result = "Churned Customer" if pred == 0 else "Retained Customer"
-                    results_data.append([result])  # Store result in a single list
+                    row = [result]
+                    results_data.append(row)
 
-                # Create a DataFrame with one column for predictions
-                results_df = pd.DataFrame(results_data, columns=["Prediction"])
+                # Create a DataFrame without the Index column
+                results_df = pd.DataFrame(results_data, columns=["Binary Prediction", "Detailed Prediction"])
 
                 # Display the DataFrame in Streamlit without index
                 results_placeholder.write(results_df)
@@ -69,6 +74,6 @@ if uploaded_file is not None:
 # About section with updated information
 st.sidebar.title("About")
 st.sidebar.info("""
-This app classifies the data from Waze using a pre-trained XGBoost model to determine if a customer is predicted to be churned or not.
-Upload a CSV file with test data (10 columns for features) and receive a prediction.
+This app classifies the data from Waze cmopany using a pre-trained XGBoost model to determine if a customer is predicted to be churned or not.
+Upload a CSV file with test data (12 columns for features) and receive a prediction.
 """)
