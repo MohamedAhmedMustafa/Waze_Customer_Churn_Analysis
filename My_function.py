515b41ef3d4c4c947652ca5c922821ee8dd99484
Mohamed Ahmed Mustafa <mohamedahmedmustafa22@gmail.com>
2024-10-12 23:07:09 +0300

    Update My_function.py

--- My_function.py
+++ My_function.py
@@ -1,12 +1,12 @@
 import pandas as pd
 def preprocessing_df(df):
-  df[12] = pd.Categorical(df[12]).codes
+  df[11] = pd.Categorical(df[11]).codes
   df = df.drop(columns=[0])
   df = df.dropna()
   percentiles_99 = df.quantile(0.95)
   # Filter out records where values exceed the 99th percentile in any column
   df_filtered = df[(df <= percentiles_99).all(axis=1)]
   df = df_filtered
-  df = df.drop(columns=[12])
+  df = df.drop(columns=[11])
   df = df.dropna()
   return df
